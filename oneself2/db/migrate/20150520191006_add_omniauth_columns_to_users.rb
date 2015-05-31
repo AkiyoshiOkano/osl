a0b2cafa517b6967b01ee41dcf60ee62c3c1305b
AkiyoshiOkano <akiyoshiokano0415@example.com>
2015-05-31 11:21:29 +0900

    setting paper model

--- oneself2/db/migrate/20150520191006_add_omniauth_columns_to_users.rb
+++ oneself2/db/migrate/20150520191006_add_omniauth_columns_to_users.rb
@@ -3,7 +3,6 @@ class AddOmniauthColumnsToUsers < ActiveRecord::Migration
     add_column :users, :uid,      :string, null: false, default: ""
     add_column :users, :provider, :string, null: false, default: ""
     add_column :users, :name,     :string
-    add_column :users, :image,     :string
 
     add_index :users, [:uid, :provider], unique: true
   end
